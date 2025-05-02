b31454016c1bf1ab0bd2eb83ff2f896bda5ec528
Ranil <Ranil@DESKTOP-V42TCMT>
2025-05-02 06:23:56 +0530

    commit and push

--- 7rmartSupermarket/src/main/java/pages/ManageNewsPage.java
+++ 7rmartSupermarket/src/main/java/pages/ManageNewsPage.java
@@ -1,5 +1,50 @@
 package pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class ManageNewsPage {
 
+	public WebDriver driver;
+
+	public ManageNewsPage(WebDriver driver) {
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+	}
+	
+	
+ @FindBy(xpath="//p[text()='Manage News']")private WebElement manageNews;
+ @FindBy(xpath="//a[text()=' New']")private WebElement newNews;
+ @FindBy(id="news")private WebElement entertheNews;
+ @FindBy(xpath="//button[@name='create']")private WebElement savebutton;
+ @FindBy(xpath="//a[normalize-space()='Search']")private WebElement search;
+ @FindBy(xpath="//input[@name='un']")private WebElement searchNews;
+ @FindBy(xpath="//button[@name='Search']")private WebElement searchsubmittbutton;
+ 
+ 
+ 
+ public void clickOnManageNews() {
+	 manageNews.click();
+ }
+ public void clickOnNewinManageNews() {
+	 newNews.click();
+ }
+ public void enteraNewsOnEnterTheNews(String news) {
+	 entertheNews.sendKeys(news);
+ }
+ public void clickOnSaveButtonInEnterNewsInfo() {
+	 savebutton.click();
+ }
+ public void clickOnSearchButton() {
+	 search.click();
+ }
+ public void entertheNewsOnSearchManageNews(String news1) {
+	 searchNews.sendKeys(news1);
+ }
+ public void clickOnSearchButtonInSearchMangeNews() {
+	 searchsubmittbutton.click();
+ }
+	
 }
--- 7rmartSupermarket/src/test/java/testScript/ManageNewsTest.java
+++ 7rmartSupermarket/src/test/java/testScript/ManageNewsTest.java
@@ -1,5 +1,46 @@
 package testScript;
 
-public class ManageNewsTest {
+import java.io.IOException;
+
+import org.testng.annotations.Test;
+
+import automationCore.Base;
+import pages.LoginPage;
+import pages.ManageNewsPage;
+import utilities.ExcelUtility;
+
+public class ManageNewsTest extends Base {
+	
+	@Test
+	public void verifyUsercanAddNews() throws IOException {
+		String username=ExcelUtility.readStringData(0, 0, "LoginPage");
+		String password=ExcelUtility.readStringData(0, 1, "LoginPage");
+		LoginPage login=new LoginPage(driver);
+		login.enterUsernameOnUsernameField(username);
+		login.enterPasswordOnPasswordField(password);
+		login.clickOnSigninbutton();
+		ManageNewsPage managenews=new ManageNewsPage(driver);
+		managenews.clickOnManageNews();
+		managenews.clickOnNewinManageNews();
+		String news=ExcelUtility.readStringData(0, 0, "ManageNewsPage");
+		managenews.enteraNewsOnEnterTheNews(news);
+		managenews.clickOnSaveButtonInEnterNewsInfo();
+		
+	}
+	@Test
+	public void verifyUsercanSearchNews() throws IOException {
+		String username=ExcelUtility.readStringData(0, 0, "LoginPage");
+		String password=ExcelUtility.readStringData(0, 1, "LoginPage");
+		LoginPage login=new LoginPage(driver);
+		login.enterUsernameOnUsernameField(username);
+		login.enterPasswordOnPasswordField(password);
+		login.clickOnSigninbutton();
+		ManageNewsPage managenews=new ManageNewsPage(driver);
+		managenews.clickOnManageNews();
+		managenews.clickOnSearchButton();
+		String news1=ExcelUtility.readStringData(0, 0, "ManageNewsPage");
+		managenews.entertheNewsOnSearchManageNews(news1);
+		managenews.clickOnSearchButtonInSearchMangeNews();
+	}
 
 }
